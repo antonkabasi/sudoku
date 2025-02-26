5cb85064259f2ed2b66b086fba99535684970eb7
antonkabasi <anton.kabasi.gm@gmail.com>
2025-02-26 17:26:06 +0100

    front end almost done

--- SudokuMVC/Controllers/HomeController.cs
+++ SudokuMVC/Controllers/HomeController.cs
@@ -1,31 +1,67 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using SudokuMVC.Models;
+using YourProjectNamespace.Models;
+using System.Globalization;
 
-namespace SudokuMVC.Controllers;
-
-public class HomeController : Controller
+namespace YourProjectNamespace.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        // Main page: generates a new game and starts the stopwatch automatically.
+        public IActionResult Index(string difficulty)
+        {
+            var game = new SudokuGame();
+            if (!string.IsNullOrEmpty(difficulty))
+            {
+                game.GeneratePuzzle(difficulty);
+            }
+            else
+            {
+                game.GeneratePuzzle("medium");
+            }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+            // Reset and start the stopwatch automatically.
+            ResetStopwatchInternal();
+            StartStopwatchInternal();
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+            return View(game);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // Endpoint to return the current stopwatch elapsed time (in seconds).
+        public IActionResult StopwatchTime()
+        {
+            // Retrieve stored stopwatch data.
+            string stopwatchRunning = HttpContext.Session.GetString("StopwatchRunning") ?? "false";
+            string stopwatchStartString = HttpContext.Session.GetString("StopwatchStart");
+            string stopwatchElapsedString = HttpContext.Session.GetString("StopwatchElapsed") ?? "0";
+            int storedElapsed = int.Parse(stopwatchElapsedString);
+            int totalElapsed = storedElapsed;
+
+            // If running, add the elapsed time since the stopwatch started.
+            if (stopwatchRunning == "true" && !string.IsNullOrEmpty(stopwatchStartString))
+            {
+                DateTime stopwatchStart = DateTime.Parse(stopwatchStartString, null, DateTimeStyles.RoundtripKind);
+                totalElapsed += (int)(DateTime.UtcNow - stopwatchStart).TotalSeconds;
+            }
+            return Json(new { elapsed = totalElapsed });
+        }
+
+        #region Stopwatch Helper Methods
+
+        // Resets the stopwatch session data.
+        private void ResetStopwatchInternal()
+        {
+            HttpContext.Session.SetString("StopwatchElapsed", "0");
+            HttpContext.Session.Remove("StopwatchStart");
+            HttpContext.Session.SetString("StopwatchRunning", "false");
+        }
+
+        // Starts the stopwatch by recording the current time.
+        private void StartStopwatchInternal()
+        {
+            HttpContext.Session.SetString("StopwatchStart", DateTime.UtcNow.ToString("o"));
+            HttpContext.Session.SetString("StopwatchRunning", "true");
+        }
+
+        #endregion
     }
 }
---
+++ SudokuMVC/Models/SudokuGame.cs
@@ -0,0 +1,51 @@
+namespace YourProjectNamespace.Models
+{
+    public class SudokuGame
+    {
+        public int[,] Board { get; set; }
+        public string Difficulty { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public SudokuGame()
+        {
+            Board = new int[9, 9];
+            ClearBoard();
+            StartTime = DateTime.UtcNow; // Set the game start time
+        }
+
+        public void ClearBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Board[i, j] = 0;
+                }
+            }
+        }
+
+        // Generates a puzzle based on the difficulty level.
+        public void GeneratePuzzle(string difficulty)
+        {
+            Difficulty = difficulty;
+            ClearBoard();
+
+            // Example stub: fill a certain number of cells based on difficulty
+            int cellsToFill = difficulty.ToLower() switch
+            {
+                "easy" => 40,
+                "medium" => 30,
+                "hard" => 20,
+                _ => 30,
+            };
+
+            Random rnd = new Random();
+            for (int n = 0; n < cellsToFill; n++)
+            {
+                int i = rnd.Next(0, 9);
+                int j = rnd.Next(0, 9);
+                Board[i, j] = rnd.Next(1, 10);
+            }
+        }
+    }
+}
